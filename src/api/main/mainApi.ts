94d386114f0691f9c3253c675efb85008fd725d3
hyeon-aa <ahhyun1120@naver.com>
2024-12-17 22:58:15 +0900

    api axios 연결 세팅

---
+++ src/api/main/mainApi.ts
@@ -0,0 +1,6 @@
+import { http } from "../request";
+
+export const main = {
+
+    
+}
---
+++ src/api/request.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import {
+	AxiosInstance,
+	AxiosError,
+	InternalAxiosRequestConfig,
+	AxiosResponse,
+} from "axios";
+
+const BASE_PATH = "";
+
+const Instance = (): AxiosInstance => {
+	const instance: AxiosInstance = axios.create({
+		baseURL: BASE_PATH,
+		headers: {
+			"Content-type": "application/json",
+		},
+	});
+
+	instance.interceptors.request.use(
+		(config: InternalAxiosRequestConfig) => {
+			if (!config.headers) {
+				return config;
+			}
+
+			const authToken = localStorage.getItem("token");
+
+			if (authToken) {
+				config.headers.Authorization = `Bearer ${authToken}`;
+			}
+			return config;
+		},
+		(error: AxiosError) => {
+			console.log(error);
+			return Promise.reject(error);
+		},
+	);
+
+	instance.interceptors.response.use(
+		(response: AxiosResponse) => {
+			return response;
+		},
+		(error: AxiosError) => {
+			// if () {
+				
+			// }
+			return Promise.reject(error);
+		},
+	);
+
+	return instance;
+};
+
+export const http = Instance();
