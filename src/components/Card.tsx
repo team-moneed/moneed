734582e5de434d3450503da6511ac15250f2f349
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-04-25 20:52:26 +0900

    [Fix] 에러 해결 - 내용 - 1. import 에러 해결 2. each child should have unique key 에러 해결 3. Cannot use 'import.meta' outside a module 에러 해결 - 원인 - 1. 잘못된 경로 2. map 사용시 key에 index 사용 3. module이 아닌 script 태그에서 import문 사용

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 type CardProps = {
     cardImgages: string[] | string;
@@ -10,30 +10,27 @@ type CardProps = {
     className?: string;
 };
 
-const Card = ({ cardImgages, title, content, date, children, className = "", onClick }: CardProps) => {
-
+const Card = ({ cardImgages, title, content, date, children, className = '', onClick }: CardProps) => {
     const images = Array.isArray(cardImgages) ? cardImgages : [cardImgages];
 
     return (
-        <div
-            onClick={onClick}
-            className={`cursor-pointer ${className}`}
-        >
+        <div onClick={onClick} className={`cursor-pointer ${className}`}>
             {images.map((cardImgage, index) => (
-                <div className="aspect-[350/228] lg:aspect-[303/195] rounded-[1.6rem] overflow-hidden">
-                    <img
-                        key={index}
-                        src={cardImgage}
-                        alt={`Card image ${index + 1}`}
-                        className="w-full h-full object-cover"
-                    />
+                <div key={cardImgage} className='aspect-[350/228] lg:aspect-[303/195] rounded-[1.6rem] overflow-hidden'>
+                    <img src={cardImgage} alt={`Card image ${index + 1}`} className='w-full h-full object-cover' />
                 </div>
             ))}
 
-            <div className="flex gap-[1.2rem] flex-col mt-[1.2rem]">
-                {date && <p className="text-[var(--moneed-gray-7)] text-[1.2rem] leading-[135%]">{date}</p>}
-                {title && <h3 className="text-[1.8rem] text-[var(--moneed-black)] font-semibold leading-[135%] line-clamp-2">{title}</h3>}
-                {content && <p className="text-[var(--moneed-gray-7)] text-[1.4rem] leading-[142%] line-clamp-2">{content}</p>}
+            <div className='flex gap-[1.2rem] flex-col mt-[1.2rem]'>
+                {date && <p className='text-[var(--moneed-gray-7)] text-[1.2rem] leading-[135%]'>{date}</p>}
+                {title && (
+                    <h3 className='text-[1.8rem] text-[var(--moneed-black)] font-semibold leading-[135%] line-clamp-2'>
+                        {title}
+                    </h3>
+                )}
+                {content && (
+                    <p className='text-[var(--moneed-gray-7)] text-[1.4rem] leading-[142%] line-clamp-2'>{content}</p>
+                )}
             </div>
 
             {children}
--- src/components/Carousel/PostCarousel.tsx
+++ src/components/Carousel/PostCarousel.tsx
@@ -1,33 +1,33 @@
-import { EmblaOptionsType } from 'embla-carousel'
-import useEmblaCarousel from 'embla-carousel-react'
-import { usePrevNextButtons, NextButton } from './CarouselArrowButton'
-import CommunityThumbnailCard from '../Community/CommunityThumbnailCard'
+import { EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import { NextButton } from './CarouselArrowButton';
+import { usePrevNextButtons } from '@/hook/usePrevNextButtons';
+import CommunityThumbnailCard from '../Community/CommunityThumbnailCard';
 
 type PropType = {
     slides: {
         postId: number;
-        userName: string
-        content: string
-        title: string
-        createdAt: string
+        userName: string;
+        content: string;
+        title: string;
+        createdAt: string;
         stocktype?: string;
-    }[]
-    options?: EmblaOptionsType
-}
+    }[];
+    options?: EmblaOptionsType;
+};
 
 const PostCarousel: React.FC<PropType> = ({ slides, options }) => {
-    const [emblaRef, emblaApi] = useEmblaCarousel(options)
-    const { nextBtnDisabled, onNextButtonClick } =
-        usePrevNextButtons(emblaApi)
+    const [emblaRef, emblaApi] = useEmblaCarousel(options);
+    const { nextBtnDisabled, onNextButtonClick } = usePrevNextButtons(emblaApi);
 
     return (
-        <div className="relative lg:pr-[5.6rem]">
-            <div className="w-full overflow-hidden mask-right" ref={emblaRef}>
-                <div className="flex gap-[1.6rem]">
-                    {slides.map((slide) => (
+        <div className='relative lg:pr-[5.6rem]'>
+            <div className='w-full overflow-hidden mask-right' ref={emblaRef}>
+                <div className='flex gap-[1.6rem]'>
+                    {slides.map(slide => (
                         <div
                             key={slide.postId}
-                            className="flex-shrink-0 w-[calc(85%_-_1.6rem)] lg:w-[calc(50%_+_.8rem)]"
+                            className='flex-shrink-0 w-[calc(85%_-_1.6rem)] lg:w-[calc(50%_+_.8rem)]'
                         >
                             <CommunityThumbnailCard
                                 // postId={slide.postId}
@@ -48,10 +48,10 @@ const PostCarousel: React.FC<PropType> = ({ slides, options }) => {
             <NextButton
                 onClick={onNextButtonClick}
                 disabled={nextBtnDisabled}
-                className="hidden lg:absolute lg:block top-1/2 right-2 transform -translate-y-1/2 z-10 p-[1.2rem] rounded-[2rem] bg-[var(--moneed-gray-5)]"
+                className='hidden lg:absolute lg:block top-1/2 right-2 transform -translate-y-1/2 z-10 p-[1.2rem] rounded-[2rem] bg-[var(--moneed-gray-5)]'
             />
         </div>
-    )
-}
+    );
+};
 
-export default PostCarousel
+export default PostCarousel;
