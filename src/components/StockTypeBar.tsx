24bc8bfa51db1405b9ba83ae8bdffaed6dcab059
JaeHye0k <ahhpc2012@gmail.com>
2025-05-19 17:02:07 +0900

    [Style] + 버튼 스타일 수정

--- src/components/StockTypeBar.tsx
+++ src/components/StockTypeBar.tsx
@@ -4,6 +4,7 @@ import Chip from '@/components/Chip';
 import { StockType } from '@/types/stock';
 import { useQuery } from '@tanstack/react-query';
 import { useParams, useRouter } from 'next/navigation';
+import Icon from './Icon';
 
 const StockTypeBar = () => {
     //선택한 카테고리만 보이게
@@ -25,8 +26,10 @@ const StockTypeBar = () => {
 
     return (
         <div className='relative'>
-            <div className='flex gap-4 mb-6 overflow-x-auto whitespace-nowrap'>
-                <Chip label='+' onClick={movetoSelectStockType} />
+            <div className='flex gap-4 mb-6 overflow-x-auto whitespace-nowrap items-center'>
+                <button onClick={movetoSelectStockType} className='shrink-0'>
+                    <Icon iconUrl='/icon/icon-addcircle.svg' width={30} height={30} />
+                </button>
                 {stockTypes?.map(({ stocktype, StockTypeId }) => (
                     <Chip
                         key={StockTypeId}
