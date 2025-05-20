338a1277e284ea444500a06c04bd989d55244fb7
JaeHye0k <ahhpc2012@gmail.com>
2025-05-20 13:10:18 +0900

    [Fix] window is not defined 엥러 해결

--- src/hooks/useMoveScroll.ts
+++ src/hooks/useMoveScroll.ts
@@ -3,8 +3,8 @@ import { RefObject } from 'react';
 function useMoveScroll(ref: RefObject<HTMLDivElement>) {
     const headerOffset = 70;
     const elementPositionTop = ref.current?.getBoundingClientRect().top;
-    const offsetPosition = (elementPositionTop || 0) + window.scrollY - headerOffset;
     const onMoveToElement = () => {
+        const offsetPosition = (elementPositionTop || 0) + window.scrollY - headerOffset;
         window.scrollTo({
             top: offsetPosition,
             behavior: 'smooth',
