d0859fba195a5029d8a56510a1efdffa635966ce
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-06-16 09:24:42 +0900

    [Fix] 모바일 네비게이션 스타일 깨짐 수정

--- src/components/Layout/MobileNav.tsx
+++ src/components/Layout/MobileNav.tsx
@@ -29,30 +29,28 @@ const MobileNav = () => {
     };
 
     return (
-        <>
-            <div className='flex fixed bottom-0 left-0 right-0 z-10 pt-[.6rem] pb-4 px-8 bg-moneed-gray-3 xl:hidden'>
-                <NavLink href='/' icon='/icon/icon-m-nav-1.svg' activeIcon='/icon/icon-lnb-1-on.svg'>
-                    홈페이지
-                </NavLink>
-                <NavLink href='/shortform' icon='/icon/icon-m-nav-2.svg' activeIcon='/icon/icon-lnb-2-on.svg'>
-                    숏폼
-                </NavLink>
-                <NavLink href='/community' icon='/icon/icon-m-nav-3.svg' activeIcon='/icon/icon-lnb-3-on.svg'>
-                    커뮤니티
-                </NavLink>
-                <NavLink href='/mypage' icon='/icon/icon-lnb-4.svg' activeIcon='/icon/icon-lnb-4-on.svg'>
-                    내프로필
-                </NavLink>
+        <div className='flex justify-between fixed bottom-0 left-0 right-0 z-10 pt-[.6rem] pb-4 px-8 bg-moneed-gray-3 sm:hidden'>
+            <NavLink href='/' icon='/icon/icon-m-nav-1.svg' activeIcon='/icon/icon-lnb-1-on.svg'>
+                홈페이지
+            </NavLink>
+            <NavLink href='/shortform' icon='/icon/icon-m-nav-2.svg' activeIcon='/icon/icon-lnb-2-on.svg'>
+                숏폼
+            </NavLink>
+            <NavLink href='/community' icon='/icon/icon-m-nav-3.svg' activeIcon='/icon/icon-lnb-3-on.svg'>
+                커뮤니티
+            </NavLink>
+            <NavLink href='/mypage' icon='/icon/icon-lnb-4.svg' activeIcon='/icon/icon-lnb-4-on.svg'>
+                내프로필
+            </NavLink>
 
-                <button
-                    type='button'
-                    onClick={motetowritepost}
-                    className='aspect-square w-[5.2rem] bg-moneed-brand absolute bottom-[calc(100%+2rem)] flex items-center justify-center rounded-full right-8'
-                >
-                    <img src='/icon/icon-edit.svg' alt='' />
-                </button>
-            </div>
-        </>
+            <button
+                type='button'
+                onClick={motetowritepost}
+                className='aspect-square w-[5.2rem] bg-moneed-brand absolute bottom-[calc(100%+2rem)] flex items-center justify-center rounded-full right-8'
+            >
+                <img src='/icon/icon-edit.svg' alt='' />
+            </button>
+        </div>
     );
 };
 
