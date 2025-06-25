51c69664cde251ae112289067ce01ad0fbfa4969
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-06-25 15:22:00 +0900

    [Feat] Top5 게시글 조회 기능 구현

--- src/app/community/Top5.tsx
+++ src/app/community/Top5.tsx
@@ -1,46 +1,12 @@
+'use client';
+
+import { getTopPosts } from '@/api/post.api';
 import PostCarousel from '@/components/Carousel/PostCarousel';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
-// TODO: API 추가
-const topPosts = [
-    {
-        postId: 1,
-        title: '1',
-        content:
-            '1 테슬라 주식 언제 사테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사 테슬라 주식 언제 사  ',
-        userName: '사용자1',
-        createdAt: '2024-12-10T10:00:00Z',
-        stocktype: '테슬라',
-    },
-    {
-        postId: 2,
-        title: '2',
-        content:
-            '2 주식으로 돈벌래돈벌꺼야!!테슬라 주식 언제 사테슬라 주식 언제 사테슬라 주식 언제 사테슬라 주식 언제 사테슬라 주식 언제 사테슬라 주식 언제 사테슬라 주식 언제 사언제사야이득이야? 알려줘알려줘알려주라고!!',
-        userName: '사용자5',
-        createdAt: '2024-12-09T09:00:00Z',
-        stocktype: '애플',
-    },
-    {
-        postId: 3,
-        title: '3',
-        content: '3 카카오게시글! 카카오카카ㅗ오오',
-        userName: '사용자6',
-        createdAt: '2024-12-09T09:00:00Z',
-        stocktype: '카카오',
-    },
-    {
-        postId: 4,
-        title: '4',
-        content: '4 카카오게시글! 카카오카카ㅗ오오',
-        userName: '사용자6',
-        createdAt: '2024-12-09T09:00:00Z',
-        stocktype: '카카오',
-    },
-];
-
-const Top5 = ({ ref }: { ref: React.RefObject<HTMLDivElement> }) => {
+const Top5 = () => {
     const title = 'Top 5';
-    const standardDate = '12월';
+    const standardDate = new Date().toLocaleDateString('ko-KR', { month: 'long' });
     const POSTOPTIONS = {
         slidesToScroll: 1,
         loop: false,
@@ -49,17 +15,20 @@ const Top5 = ({ ref }: { ref: React.RefObject<HTMLDivElement> }) => {
         // containScroll: "keepSnaps",
     };
 
+    const { data: topPosts } = useSuspenseQuery({
+        queryKey: ['top-posts'],
+        queryFn: () => getTopPosts({ limit: 5 }),
+    });
+
     return (
-        <section ref={ref} className='mt-[3.6rem]'>
+        <section id='top5' className='mt-[3.6rem]'>
             <div className='flex items-baseline gap-[.8rem] mb-[1.6rem]'>
-                <h2 className='text-[2.2rem] leading-[145%] font-bold text-(--moneed-black) lg:text-[2.4rem] lg:leading-[140%]'>
+                <h2 className='text-[2.2rem] leading-[145%] font-bold text-moneed-black sm:text-2xl sm:leading-[140%]'>
                     {title}
                 </h2>
-                <span className='text-(--moneed-gray-7) text-[1.2rem] font-normal leading-[135%]'>
-                    {standardDate} 기준
-                </span>
+                <span className='text-moneed-gray-7 text-[1.2rem] font-normal leading-[135%]'>{standardDate} 기준</span>
             </div>
-            <PostCarousel slides={topPosts} options={POSTOPTIONS} />
+            <PostCarousel posts={topPosts} options={POSTOPTIONS} />
         </section>
     );
 };
--- src/components/Carousel/PostCarousel.tsx
+++ src/components/Carousel/PostCarousel.tsx
@@ -1,53 +1,77 @@
+'use client';
 import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
-import { NextButton } from '@/components/Carousel/CarouselArrowButton';
+import { NextButton, PrevButton } from '@/components/Carousel/CarouselArrowButton';
 import { usePrevNextButtons } from '@/hooks/usePrevNextButtons';
 import CommunityThumbnailCard from '@/components/Community/CommunityThumbnailCard';
+import { Suspense } from 'react';
+import { PostSkeleton } from '@/components/Community/Post';
+import { PostUser } from '@/types/post';
+import { cn } from '@/util/style';
 
 type PropType = {
-    slides: {
-        postId: number;
-        userName: string;
+    posts: {
+        id: number;
         content: string;
         title: string;
         createdAt: string;
         stocktype?: string;
+        user: PostUser;
     }[];
     options?: EmblaOptionsType;
 };
 
-const PostCarousel: React.FC<PropType> = ({ slides, options }) => {
+const PostCarousel: React.FC<PropType> = ({ posts, options }) => {
     const [emblaRef, emblaApi] = useEmblaCarousel(options);
-    const { nextBtnDisabled, onNextButtonClick } = usePrevNextButtons(emblaApi);
+    const { nextBtnDisabled, onNextButtonClick, prevBtnDisabled, onPrevButtonClick } = usePrevNextButtons(emblaApi);
 
     return (
         <div className='relative lg:pr-[5.6rem]'>
             <div className='w-full overflow-hidden mask-right' ref={emblaRef}>
                 <div className='flex gap-[1.6rem]'>
-                    {slides.map(slide => (
-                        <div key={slide.postId} className='shrink-0 w-[calc(85%-1.6rem)] lg:w-[calc(50%+.8rem)]'>
+                    {posts.map(post => (
+                        <div key={post.id} className='shrink-0 w-[calc(85%-1.6rem)] lg:w-[calc(50%+.8rem)]'>
                             <CommunityThumbnailCard
-                                // postId={slide.postId}
-                                userName={slide.userName}
-                                content={slide.content}
-                                title={slide.title}
-                                createdAt={slide.createdAt}
+                                userName={post.user.nickname}
+                                content={post.content}
+                                title={post.title}
+                                createdAt={post.createdAt}
                             ></CommunityThumbnailCard>
                         </div>
                     ))}
                 </div>
             </div>
-            {/* <PrevButton
+            <PrevButton
                 onClick={onPrevButtonClick}
                 disabled={prevBtnDisabled}
-                className="absolute top-1/2 left-2 transform -translate-y-1/2 z-10"
-            /> */}
+                className={cn(
+                    'hidden lg:absolute lg:block top-1/2 left-2 transform -translate-y-1/2 z-10 p-[1.2rem] rounded-4xl bg-moneed-gray-5',
+                    prevBtnDisabled && 'lg:hidden',
+                )}
+            />
             <NextButton
                 onClick={onNextButtonClick}
                 disabled={nextBtnDisabled}
-                className='hidden lg:absolute lg:block top-1/2 right-2 transform -translate-y-1/2 z-10 p-[1.2rem] rounded-4xl bg-moneed-gray-5'
+                className={cn(
+                    'hidden lg:absolute lg:block top-1/2 right-2 transform -translate-y-1/2 z-10 p-[1.2rem] rounded-4xl bg-moneed-gray-5',
+                    nextBtnDisabled && 'lg:hidden',
+                )}
             />
         </div>
+    );
+};
+
+export const PostCarouselWithSuspense = ({ posts: slides, options }: PropType) => {
+    return (
+        <Suspense
+            fallback={Array.from({ length: 5 }).map((_, index) => (
+                <div key={index} className='w-[calc(85%-1.6rem)] lg:w-[calc(50%+.8rem)]'>
+                    <PostSkeleton />
+                </div>
+            ))}
+        >
+            <PostCarousel posts={slides} options={options} />
+        </Suspense>
     );
 };
 
