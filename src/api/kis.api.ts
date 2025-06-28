62f52b3c172fc54e265c11298c0ab9b25bc3c897
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-06-28 22:35:24 +0900

    [Feat] 한투 액세스 토큰 발급 로직
    - 액세스 토큰을 메모리(변수)에 저장
    - 액세스 토큰이 없거나 만료시 재발급

--- src/api/kis.api.ts
+++ src/api/kis.api.ts
@@ -1,5 +1,7 @@
 import 'server-only';
 import { kis } from './server';
+import { KISAccessTokenResponse, OverseasStockConditionSearchResponse, MarketCode } from '@/types/kis';
+import axios from 'axios';
 
 // 한국 투자증권 API
 
@@ -8,8 +10,12 @@ type MarketCode = 'NYS' | 'NAS' | 'AMS' | 'TSE' | 'HKS' | 'SHS' | 'SZS' | 'HSX' 
 const accessTokenUrl = '/oauth2/tokenP';
 const searchByConditionUrl = '/uapi/overseas-price/v1/quotations/inquire-search';
 
+/**
+ * 접근토큰발급(P)[인증-001]
+ * {@link https://apiportal.koreainvestment.com/apiservice-apiservice?/oauth2/tokenP API DOCS}
+ */
 export const getAccessToken = async () => {
-    const response = await kis.post(accessTokenUrl, {
+    const response = await axios.post<KISAccessTokenResponse>(`${process.env.KIS_BASE_URL}${accessTokenUrl}`, {
         grant_type: 'client_credentials',
         appkey: process.env.KIS_APP_KEY,
         appsecret: process.env.KIS_APP_SECRET,
