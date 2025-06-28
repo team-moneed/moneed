ed12530943c22bf11332f481397c1aab8892ddc5
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-06-28 22:32:17 +0900

    [Refactor] 컴포넌트 분리

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,77 +1,47 @@
-'use client';
-
 import HotStocks from '@/app/community/HotStocks';
-import { useEffect, useState } from 'react';
+import CommunityTabNav from '@/app/community/CommunityTabNav';
 import Top5 from './Top5';
 import HotPosts from './HotPosts';
 import Vote from './Vote';
-import { cn } from '@/util/style';
 
 const hashObj = {
     top5: 'top5',
     category: 'category',
     vote: 'vote',
     hotPosts: 'hotPosts',
-};
+} as const;
 
 const stringToHash = (str: string) => {
     return `#${str}`;
 };
 
+const communityTabs = [
+    {
+        id: hashObj.top5,
+        label: 'Top 5',
+        href: stringToHash(hashObj.top5),
+    },
+    {
+        id: hashObj.category,
+        label: '지금 뜨는 종목',
+        href: stringToHash(hashObj.category),
+    },
+    {
+        id: hashObj.vote,
+        label: '지금 핫한 투표',
+        href: stringToHash(hashObj.vote),
+    },
+    {
+        id: hashObj.hotPosts,
+        label: '인기 급상승 게시글',
+        href: stringToHash(hashObj.hotPosts),
+    },
+];
+
 export default function CommunityPage() {
-    const [hash, setHash] = useState('');
-    const handleHashChange = () => {
-        const hash = window.location.hash;
-        setHash(hash);
-    };
-
-    useEffect(() => {
-        window.addEventListener('hashchange', handleHashChange);
-
-        return () => window.removeEventListener('hashchange', handleHashChange);
-    }, []);
     return (
         <div>
-            <div className='flex gap-4 pt-8 items-start border-b-2 border-moneed-gray-5'>
-                <a
-                    href={stringToHash(hashObj.top5)}
-                    className={cn(
-                        'text-moneed-gray-7 mr-[1.2rem] sm:text-lg sm:leading-[140%] cursor-pointer pb-[.3rem]',
-                        hash === stringToHash(hashObj.top5) && 'text-moneed-black border-b-[3px] border-b-moneed-brand',
-                    )}
-                >
-                    Top 5
-                </a>
-                <a
-                    href={stringToHash(hashObj.category)}
-                    className={cn(
-                        'text-moneed-gray-7 mr-[1.2rem] sm:text-lg sm:leading-[140%] cursor-pointer pb-[.3rem]',
-                        hash === stringToHash(hashObj.category) &&
-                            'text-moneed-black border-b-[3px] border-b-moneed-brand',
-                    )}
-                >
-                    지금 뜨는 종목
-                </a>
-                <a
-                    href={stringToHash(hashObj.vote)}
-                    className={cn(
-                        'text-moneed-gray-7 mr-[1.2rem] sm:text-lg sm:leading-[140%] cursor-pointer pb-[.3rem]',
-                        hash === stringToHash(hashObj.vote) && 'text-moneed-black border-b-[3px] border-b-moneed-brand',
-                    )}
-                >
-                    지금 핫한 투표
-                </a>
-                <a
-                    href={stringToHash(hashObj.hotPosts)}
-                    className={cn(
-                        'text-moneed-gray-7 mr-[1.2rem] sm:text-lg sm:leading-[140%] cursor-pointer pb-[.3rem]',
-                        hash === stringToHash(hashObj.hotPosts) &&
-                            'text-moneed-black border-b-[3px] border-b-moneed-brand',
-                    )}
-                >
-                    인기 급상승 게시글
-                </a>
-            </div>
+            <CommunityTabNav tabs={communityTabs} />
             <Top5 id={hashObj.top5} />
             <HotStocks id={hashObj.category} />
             <Vote id={hashObj.vote} />
