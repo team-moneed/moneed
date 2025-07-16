f8b15dfcb17184efd344817d8c46f72dc4e8658c
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-07-16 14:13:37 +0900

    [Refactor] 댓글 케밥 버튼 조건부 렌더링 (내 댓글인 경우, 아닌 경우 구분)

--- src/app/community/HotPostThumbnail.tsx
+++ src/app/community/HotPostThumbnail.tsx
@@ -1,11 +1,18 @@
 import PostThumbnailCard from '@/components/PostThumbnailCard';
 import { HotPostThumbnail as THotPostThumbnail } from '@/types/post';
+import { getCookie } from '@/util/cookie';
+import { TOKEN_KEY } from '@/constants/token';
 import { EmblaOptionsType } from 'embla-carousel';
 import { useRouter } from 'next/navigation';
+import { decodeJwt } from 'jose';
+import { TokenPayload } from '@/types/auth';
+import { useEffect, useState } from 'react';
 
 export default function HotPostThumbnail({ post }: { post: THotPostThumbnail }) {
     const { user, content, isLiked, id, thumbnailImage, likeCount, createdAt, title, commentCount } = post;
     const postImages = thumbnailImage ? [thumbnailImage] : [];
+    const [decodedToken, setDecodedToken] = useState<TokenPayload | null>(null);
+    const isMyPost = decodedToken?.userId === user.id;
 
     const OPTIONS: EmblaOptionsType = {
         slidesToScroll: 1,
@@ -20,12 +27,19 @@ export default function HotPostThumbnail({ post }: { post: THotPostThumbnail }) 
         router.push(`/posts/${postId}`);
     };
 
+    useEffect(() => {
+        const accessToken = getCookie(TOKEN_KEY.ACCESS_TOKEN);
+        if (accessToken) {
+            setDecodedToken(decodeJwt<TokenPayload>(accessToken));
+        }
+    }, []);
+
     return (
         <>
             <PostThumbnailCard onClick={() => movetoDetail({ postId: id })}>
                 <PostThumbnailCard.Header>
                     <PostThumbnailCard.AuthorWithDate user={user} createdAt={new Date(createdAt)} />
-                    <PostThumbnailCard.Dropdown post={post} />
+                    {isMyPost && <PostThumbnailCard.Dropdown post={post} />}
                 </PostThumbnailCard.Header>
                 <PostThumbnailCard.Body>
                     <PostThumbnailCard.Title title={title} />
