6202e20f2f796a94ebfd63ec61ff0adac5936362
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-07-30 17:13:20 +0900

    [Feat] 회원탈퇴 플로우 반응형 구현

--- src/app/leave/page.tsx
+++ src/app/leave/page.tsx
@@ -24,9 +24,14 @@ export default function Leave() {
     });
 
     return (
-        <div className='flex flex-col items-center justify-between h-full w-full'>
-            <div className='flex flex-col items-start sm:items-center w-full'>
-                <h1 className='text-[2.4rem] font-bold leading-[140%]'>우리, 여기서 끝인가요...?</h1>
+        <div className='flex flex-col items-center justify-between h-full w-full sm:pt-[10rem] pt-[1rem] sm:pb-[4.8rem] pb-[2.4rem]'>
+            <div className='flex flex-col items-start w-full'>
+                <div className='sm:flex hidden w-full items-center justify-start'>
+                    <div className='bg-moneed-black rounded-full size-[6rem] flex items-center justify-center'>
+                        <img src='/icon/icon-logo.svg' alt='logo' className='size-[3.6rem]' />
+                    </div>
+                </div>
+                <h1 className='text-[2.4rem] font-bold leading-[140%] sm:mt-[2rem]'>우리, 여기서 끝인가요...?</h1>
                 <p className='text-md leading-[145%] mt-[1.2rem]'>
                     함께해주셔서 감사했어요. <br />
                     혹시 탈퇴 이유를 들려주실 수 있을까요?
@@ -38,7 +43,7 @@ export default function Leave() {
                     지금 탈퇴하면, 모든 기록이 사라져요
                 </p>
                 <Button
-                    variant='primary'
+                    variant='danger'
                     disabled={selectedReason === 0}
                     className='text-[1.6rem] font-bold leading-[140%] py-[1.8rem] w-full mt-[1rem]'
                     onClick={() => leaveKakao(LEAVE_REASON[selectedReason].reason || '')}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,6 +13,7 @@ const buttonVariants = cva('rounded-[1.6rem] disabled:pointer-events-none disabl
                 'bg-moneed-white text-moneed-gray-9 hover:bg-moneed-gray-5 active:bg-moneed-gray-5 border-[1.5px] border-moneed-gray-5 disabled:bg-moneed-gray-4 disabled:text-moneed-gray-6',
             ghost: 'bg-moneed-white text-moneed-gray-7 border-[1.5px] border-moneed-gray-6 disabled:bg-moneed-gray-4 disabled:text-moneed-gray-6',
             brand: 'bg-moneed-brand text-moneed-black hover:bg-moneed-brand-point active:bg-moneed-brand-point disabled:bg-moneed-gray-6 disabled:text-moneed-white',
+            danger: 'bg-moneed-red-light text-moneed-red hover:bg-moneed-error-point active:bg-moneed-error-point disabled:bg-moneed-gray-4 disabled:text-moneed-gray-6',
         },
     },
     defaultVariants: {
