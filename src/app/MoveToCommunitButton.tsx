d423f77c7935f13ef30916edab776c0bb07b071f
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-07-30 16:43:21 +0900

    [Fix] MoveToCommunityButton 스타일 깨짐 수정

--- src/app/MoveToCommunitButton.tsx
+++ src/app/MoveToCommunitButton.tsx
@@ -10,18 +10,14 @@ export default function MoveToCommunityButton({ selectedStock }: { selectedStock
     };
 
     return (
-        <div>
-            <div className='flex justify-center sm:inline-block border-moneed-gray-5 border-[.1rem] py-[2.1rem] px-[4.1rem] rounded-[1.6rem] '>
-                <Button
-                    variant='ghost'
-                    onClick={() => movecommunity(selectedStock.stockId!)}
-                    className='flex items-center gap-[.8rem] py-0 sm:pl-0'
-                >
-                    <span className='text-[1.4rem] text-moneed-gray-8 font-semibold leading-[135%]'>
-                        {selectedStock.stockName} 게시판 더보기
-                    </span>
-                </Button>
-            </div>
-        </div>
+        <Button
+            variant='secondary'
+            onClick={() => movecommunity(selectedStock.stockId!)}
+            className='flex items-center gap-[.8rem] py-[2.1rem] px-[4.1rem]'
+        >
+            <span className='text-[1.4rem] text-moneed-gray-8 font-semibold leading-[135%]'>
+                {selectedStock.stockName} 게시판 더보기
+            </span>
+        </Button>
     );
 }
