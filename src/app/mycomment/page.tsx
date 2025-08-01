1250a1d5c88f45a3529b298614c9c53e0a488d7b
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-08-01 13:50:56 +0900

    [Refactor] 내가 작성한 댓글 리팩토링

--- src/app/mycomment/page.tsx
+++ src/app/mycomment/page.tsx
@@ -3,7 +3,9 @@
 import { useState } from 'react';
 import Comment from '@/components/Comment/Comment';
 import { useSuspenseUser, useUserComments } from '@/queries/user.query';
+import { cn } from '@/util/style';
 
+// TODO: 삭제된 댓글 -> "삭제된 댓글입니다" 표시
 const MyComment = () => {
     const [activeTab, setActiveTab] = useState<'thisWeek' | 'notThisWeek'>('thisWeek');
 
@@ -20,76 +22,64 @@ const MyComment = () => {
         return commentDate >= startOfWeek;
     });
 
+    const currentTabComments = activeTab === 'thisWeek' ? thisweekComments : comments;
+
     return (
         <>
             <div className='px-8 max-w-512 mx-auto'>
                 <div className='flex my-[1.8rem]'>
                     <div className='text-[1.8rem] font-semibold leading-[140%]'>댓글</div>
                     <div className='ml-[.4rem] text-[1.8rem] font-semibold leading-[140%]'>
-                        {activeTab === 'thisWeek' ? thisweekComments.length : comments.length}
+                        {currentTabComments.length}
                     </div>
                 </div>
                 <div className='border-b-2 border-solid border-moneed-gray-5'>
                     <button
                         onClick={() => setActiveTab('thisWeek')}
-                        className={`${
+                        className={cn(
+                            'pr-[1.2rem] text-[1.6rem] font-semibold leading-[140%]',
                             activeTab === 'thisWeek'
                                 ? 'text-moneed-black border-b-4 border-solid border-moneed-black'
-                                : 'text-moneed-gray-7'
-                        } pr-[1.2rem] text-[1.6rem] font-semibold leading-[140%]`}
+                                : 'text-moneed-gray-7',
+                        )}
                     >
                         이번주 게시글
                     </button>
                     <button
                         onClick={() => setActiveTab('notThisWeek')}
-                        className={`${
+                        className={cn(
+                            'pr-[1.2rem] text-[1.6rem] font-semibold leading-[140%]',
                             activeTab === 'notThisWeek'
                                 ? 'text-moneed-black border-b-4 border-solid border-moneed-black'
-                                : 'text-moneed-gray-7'
-                        } pr-[1.2rem] text-[1.6rem] font-semibold leading-[140%]`}
+                                : 'text-moneed-gray-7',
+                        )}
                     >
                         이전 게시글
                     </button>
                 </div>
-                {activeTab === 'thisWeek' ? (
-                    <div className='pt-[1.8rem]'>
-                        {thisweekComments.map(comment => {
-                            const myComment = {
-                                ...comment,
-                                user,
-                            };
-                            return (
-                                <div key={comment.id} className='pt-[1.8rem]'>
-                                    <Comment
-                                        comment={myComment}
-                                        setEditContent={() => {}}
-                                        setIsEdit={() => {}}
-                                        setEditCommentId={() => {}}
-                                    />
-                                </div>
-                            );
-                        })}
-                    </div>
-                ) : (
-                    <div className='pt-[1.8rem]'>
-                        {comments.map(comment => {
-                            const myComment = {
-                                ...comment,
-                                user,
-                            };
-                            return (
-                                <div key={comment.id} className='pt-[1.8rem]'>
-                                    <Comment
-                                        comment={myComment}
-                                        setEditContent={() => {}}
-                                        setIsEdit={() => {}}
-                                        setEditCommentId={() => {}}
-                                    />
-                                </div>
-                            );
-                        })}
-                    </div>
-                )}
+                <div className='pt-[1.8rem]'>
+                    {currentTabComments.map(comment => {
+                        const myComment = {
+                            ...comment,
+                            user,
+                        };
+                        return (
+                            <div key={comment.id} className='pt-[1.8rem]'>
+                                <Comment
+                                    comment={myComment}
+                                    setEditContent={() => {}}
+                                    setIsEdit={() => {}}
+                                    setEditCommentId={() => {}}
+                                />
+                            </div>
+                        );
+                    })}
+                    {currentTabComments.length === 0 && (
+                        <div className='text-[1.6rem] font-normal leading-[140%] text-moneed-gray-7'>
+                            최근에 작성한 댓글이 없어요
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     );
