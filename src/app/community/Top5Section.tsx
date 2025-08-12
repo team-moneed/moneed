d5a90b943c20e256a108a01100ba7470136c735e
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-08-12 15:01:26 +0900

    [Feat] 스맛톡 페이지 초안 작성 및 global.css 파일 수정 (헤딩 스타일 추가)

--- src/app/community/Top5Section.tsx
+++ src/app/community/Top5Section.tsx
@@ -10,9 +10,7 @@ export default function Top5Section({ id }: { id: string }) {
     return (
         <section id={id} className='mt-[3.6rem]'>
             <div className='flex items-baseline gap-[.8rem] mb-[1.6rem]'>
-                <h2 className='text-[2.2rem] leading-[145%] font-bold text-moneed-black sm:text-2xl sm:leading-[140%]'>
-                    {title}
-                </h2>
+                <h2 className='text-moneed-black text-h2 sm:text-h1'>{title}</h2>
                 <span className='text-moneed-gray-7 text-[1.2rem] font-normal leading-[135%]'>{standardDate} 기준</span>
             </div>
             <Top5WithSuspense />
